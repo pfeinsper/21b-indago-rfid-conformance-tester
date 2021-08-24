3d4ed6dd415fed9c5f45b6be2164dd13488f200f
4rfel <38050598+4rfel@users.noreply.github.com>
2021-08-24 19:18:03 -0300

    This is my code. My code is amazing.
    Co-Authored-By: Alexandreae <26481954+Alexandreae@users.noreply.github.com>
    Co-Authored-By: Lucas Leal Vale <26482702+lucaslealvale@users.noreply.github.com>
    Co-Authored-By: Bruno Signorelli Domingues <brunosd1@al.insper.edu.br>

--- fpga/RTL/FM0_encoder_tb.vhd
+++ fpga/RTL/FM0_encoder_tb.vhd
@@ -1,44 +1,70 @@
---library IEEE;
---use IEEE.std_logic_1164.all;
---
---
---entity FM0_encoder_tb is
---
---end entity FM0_encoder_tb;
---
---
---architecture tb of FM0_encoder_tb is
---
---	component FM0_encoder port (
---		clk  : in  std_logic;
---		updating_data_in   : in std_logic;
---		tari : in std_logic_vector(11 downto 0); -- 50*2000
---		data_in  : in std_logic_vector(11 downto 0);
---		
---		encoded_data_out : out std_logic_vector(15 downto 0);
---		reduced_clk_out : out std_logic;
---		);
---	end component;
---
---	signal clk : std_logic;
---	constant CLK_PERIOD : time := 20 ns;
---	signal pwm : std_logic;
---
---	begin
---
---	clk_process :process
---
---		begin
---			clk <= '0';
---			wait for clk_period/2;  --for 0.5 ns signal is '0'.
---			clk <= '1';
---			wait for clk_period/2;  --for next 0.5 ns signal is '1'.
---
---		end process;
---
---		u1 : servo port map (clk,
---									'1',
---									"00000010111011100",
---									pwm);
---									
---end tb;
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity fm0_tb is
+end entity fm0_tb;
+
+architecture tb of fm0_tb is
+
+  component fm0_encoder
+	generic (
+			-- defining size of data in and clock speed
+			clk_f      : natural := 50e6; -- Hz
+			data_width : natural := 8;
+			tari_width : natural := 16;
+			mask_width : natural := 4
+		);
+	port (
+		-- flags
+		clk : in std_logic;
+		rst : in std_logic;
+
+		-- config
+		tari : in std_logic_vector(tari_width-1 downto 0);
+
+		-- fifo data
+		is_fifo_empty    : in std_logic;
+		data_in          : in std_logic_vector((data_width + mask_width)-1 downto 0); -- format expected : ddddddddmmmm
+		request_new_data : out std_logic;
+
+		-- output
+		data_out : out std_logic
+	);
+
+ end component;
+
+	signal clk, data_out, is_fifo_empty, request_new_data : std_logic;
+	constant CLK_PERIOD : time := 20 ns;
+	constant one_package : time := 10 * 8 us;
+
+
+	begin
+
+		clk_process : process
+		begin
+			clk <= '0';
+			wait for clk_period/2;  --for 0.5 ns signal is '0'.
+			clk <= '1';
+			wait for clk_period/2;  --for next 0.5 ns signal is '1'.
+		end process;
+
+		
+		set_is_fifo_empty : process
+		begin
+			is_fifo_empty <= '0';
+			wait for one_package;
+			is_fifo_empty <= '1';
+			wait;
+		end process;
+
+
+		u0 : fm0_encoder port map (
+			clk => clk,
+			rst => '0',
+			tari => "0000000111110100", -- tari = 10 us
+			data_out => data_out,
+			is_fifo_empty => is_fifo_empty,
+			data_in => "100000101000",
+			request_new_data => request_new_data  );
+
+end tb;
