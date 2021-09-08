eb5f038bc29022d976c2cc10a8868ee7eff680f4
4rfel <38050598+4rfel@users.noreply.github.com>
2021-09-08 17:29:15 -0300

    decoder

---
+++ fpga/RTL/FM0_decoder.vhd
@@ -0,0 +1,235 @@
+
+-----------------------------------------
+--               Decoder               --
+-- Projeto Final de Engenharia         --
+-- Professor Orientador: Rafael Corsi  --
+-- Orientador: Shephard                --
+-- Alunos:                             --
+-- 		Alexandre Edington             --
+-- 		Bruno Domingues                --
+-- 		Lucas Leal                     --
+-- 		Rafael Santos                  --
+-----------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity FM0_decoder is
+	generic (
+		-- defining size of data in and clock speed
+		tari_width : natural := 16
+	);
+
+	port (
+		-- flags
+		clk : in std_logic;
+		rst : in std_logic;
+		enable : in std_logic;
+		clr_err : in std_logic;
+
+		err : out std_logic;
+		eop : out std_logic;
+
+		-- config
+		tari_101 : in std_logic_vector(tari_width-1 downto 0); -- 1% above tari
+		tari_099 : in std_logic_vector(tari_width-1 downto 0); -- 1% below tari
+		tari_1616 : in std_logic_vector(tari_width-1 downto 0); -- 1% above 1.6 tari
+		tari_1584 : in std_logic_vector(tari_width-1 downto 0); -- 1% below 1.6 tari
+
+		data_in : in std_logic;
+
+		-- output
+		data_out : out std_logic
+	);
+
+end entity;
+
+architecture arch of FM0_decoder is
+	------------------------------
+	--          values          --
+	------------------------------
+	signal tari_1616_value : integer;
+	signal tari_1584_value : integer;
+	signal tari_1010_value : integer;
+	signal tari_0990_value : integer;	
+	signal tari_0505_value : integer;
+	signal tari_0495_value : integer;
+
+	------------------------------
+	--          flags           --
+	------------------------------
+	signal data_receiver_start : std_logic := '0';
+	signal data_receiver_end   : std_logic := '0';
+
+	signal clock_start, clock_end, clear_counter, kabum_clock : std_logic := '0';
+	
+	signal prev_bit_c, prev_bit_d : std_logic := '0';
+	signal clocks_counted : integer range 0 to 1000;
+
+	------------------------------
+	--          states          --
+	------------------------------
+	type state_type_controller is (c_wait, c_decode);
+	signal state_controller	: state_type_controller := c_wait;
+	
+	type state_type_decoder is (d_wait, d_start_counter, d_stop_counter, d_continue_counter, d_error, d_pass_1_01_tari, d_counter_cs);
+	signal state_decoder     : state_type_decoder := d_wait;
+
+
+	begin
+
+		------------------------------
+		--          update          --
+		------------------------------
+		tari_1616_value <= to_integer(unsigned(tari_1616));
+		tari_1584_value <= to_integer(unsigned(tari_1584));
+		tari_1010_value <= to_integer(unsigned(tari_101));
+		tari_0990_value <= to_integer(unsigned(tari_099));	
+		tari_0505_value <= to_integer(unsigned(tari_101(tari_width-1 downto 1)));
+		tari_0495_value <= to_integer(unsigned(tari_099(tari_width-1 downto 1)));
+
+
+		decoder_controller: process ( clk, rst )
+			begin
+				if (rst = '1') then
+					state_controller  <= c_wait;
+	
+				elsif (rising_edge(clk) and enable = '1') then
+					case state_controller is
+						when c_wait =>
+							if (prev_bit_c /= data_in) then
+								prev_bit_c <= data_in;
+								state_controller <= c_decode;
+								data_receiver_start <= '1';
+							end if;
+						
+						when c_decode =>
+							if (data_receiver_end = '1') then
+								data_receiver_start <= '0';
+								state_controller <= c_wait;
+								prev_bit_c <= prev_bit_d;
+							end if;
+
+						when others =>
+							state_controller <= c_wait;
+	   				end case;
+				end if;
+		end process;
+
+		-- Decoder State Machine -> based on our diagram available in the github diagrams folder
+		decoder_data: process ( clk, rst )
+			begin
+				if (rst = '1') then
+					state_decoder  <= d_wait;
+					
+				elsif (rising_edge(clk) and enable = '1') then
+					case state_decoder is
+						when d_wait =>
+							data_receiver_end <= '0';
+							
+							if (data_receiver_start = '1') then
+								prev_bit_d <= data_in;
+								state_decoder <= d_start_counter;
+							end if;
+						-- Start Counter
+						when d_start_counter =>
+							clock_start <= '1';
+							clear_counter <= '0';
+							if (data_in /= prev_bit_d) then
+								prev_bit_d <= data_in;
+								clock_start <= '0';
+							elsif (kabum_clock = '1') then
+								clock_start <= '0';
+								state_decoder <= d_pass_1_01_tari;
+							elsif (clock_end = '1') then
+								clock_start <= '0';
+								state_decoder <= d_stop_counter;
+							end if;
+						-- Stop counter -> checks counter for a half or full tari
+						when d_stop_counter =>
+							-- checks if counted clocks is b/w 0.495tari and 0.505tari to be a 0
+							if (tari_0495_value < clocks_counted and clocks_counted < tari_0505_value) then
+								state_decoder <= d_continue_counter;
+								prev_bit_d <= data_in;
+								data_out <= '0';
+							-- checks if counted clocks is b/w 0.99tari and 1.01tari to be a 1
+                            elsif (clocks_counted > tari_0990_value and clocks_counted < tari_1010_value ) then 
+                                clear_counter <= '1';
+                                state_decoder <= d_wait;
+								data_out <= '1';
+							else
+								clock_start <= '0';
+                                clear_counter <= '1';
+								state_decoder <= d_error;
+							end if;
+						-- Continue Counter -> as half tari has passed, now we need to wait for another half tari
+                        when d_continue_counter =>
+							clock_start <= '1';
+							if (kabum_clock = '1') then
+								clock_start <= '0';
+								state_decoder <= d_pass_1_01_tari;
+							elsif(data_in /= prev_bit_d) then
+								prev_bit_d <= data_in;
+								clock_start <= '0';
+								if (tari_0990_value < clocks_counted and clocks_counted < tari_1010_value) then 
+									clear_counter <= '1';
+									state_decoder <= d_wait;
+								end if ;
+							end if ;
+						-- Error -> if there is change in the data signal before or after the margin of error, this state should handle it
+						when d_error =>
+							if (clr_err = '1') then
+								err <= '0';
+								state_decoder <= d_wait;
+							else
+								err <= '1';
+							end if ;
+						-- pass 1.01 tari -> if there is no change in the data signal, for over 1.01tari, this states signals if it is an error or an end of command.
+						when d_pass_1_01_tari =>
+							if (data_in = '1') then
+								state_decoder <= d_error;
+							else
+								state_decoder <= d_counter_cs;
+								clear_counter <= '1';
+							end if ;
+							
+						when d_counter_cs =>
+							clear_counter <= '0';
+							if (tari_1584_value < clocks_counted and clocks_counted < tari_1616_value) then
+								state_decoder <= d_wait;
+								eop <= '1';
+							else 
+								state_decoder <= d_error;
+							end if ;
+							
+						when others =>
+							state_decoder <= d_wait;
+	   				end case;
+				end if;
+		end process;
+
+		counter : process( clk, rst )
+		variable
+		begin
+			if (rst) then
+				clocks_counted <= 0;
+				clock_kabum = '0';
+				clock_end = '0';
+			elsif (rising_edge(clk)) then
+				if (clear_counter = '1') then
+					clocks_counted <= 0;
+				end if ;
+				if (clk_start = '1') then
+					clock_kabum <= '0';
+					clocks_counted <= clocks_counted + 1;
+					if (clocks_counted > tari_1010_value) then
+						clock_kabum <= '1';
+					end if ;
+				end if ;
+			end if ;
+			
+		end process ; -- counter
+
+
+end arch ; -- arch
---
+++ fpga/RTL/FM0_decoder_tb.vhd
@@ -0,0 +1,130 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity fm0_decoder_tb is
+end entity fm0_decoder_tb;
+
+architecture tb of fm0_decoder_tb is
+
+	component fm0_encoder
+		generic (
+				-- defining size of data in and clock speed
+				clk_f      : natural := 50e6; -- Hz
+				data_width : natural := 8;
+				tari_width : natural := 16;
+				mask_width : natural := 4
+		);
+		port (
+			-- flags 
+			clk : in std_logic;
+			rst : in std_logic;
+			enable : in std_logic;
+
+
+			-- config
+			tari : in std_logic_vector(tari_width-1 downto 0);
+
+			-- fifo data
+			is_fifo_empty    : in std_logic;
+			data_in          : in std_logic_vector((data_width + mask_width)-1 downto 0); -- format expected : ddddddddmmmm
+			request_new_data : out std_logic;
+
+			-- output
+			data_out : out std_logic
+		);
+
+	end component;
+
+	component fm0_decoder
+	generic (
+		-- defining size of data in and clock speed
+		tari_width : natural := 16
+	);
+
+	port (
+		-- flags
+		clk : in std_logic;
+		rst : in std_logic;
+		enable : in std_logic;
+		clr_err : in std_logic;
+
+		err : out std_logic;
+		eop : out std_logic;
+
+		-- config
+		tari_101 : in std_logic_vector(tari_width-1 downto 0); -- 1% above tari
+		tari_099 : in std_logic_vector(tari_width-1 downto 0); -- 1% below tari
+		tari_1616 : in std_logic_vector(tari_width-1 downto 0); -- 1% above 1.6 tari
+		tari_1584 : in std_logic_vector(tari_width-1 downto 0); -- 1% below 1.6 tari
+
+		data_in : in std_logic;
+
+		-- output
+		data_out : out std_logic
+	);
+
+	end component;
+
+	signal clk, eop, error_out, data_out, is_fifo_empty, request_new_data, encoded_data : std_logic := '0';
+	signal data : std_logic_vector(11 downto 0) := "111111111000";
+	constant clk_period : time := 20 ns;
+
+
+	begin
+
+		clk_process : process
+		begin
+			clk <= '0';
+			wait for clk_period/2;  --for 0.5 ns signal is '0'.
+			clk <= '1';
+			wait for clk_period/2;  --for next 0.5 ns signal is '1'.
+		end process;
+
+		fifo : process ( request_new_data )
+		variable quant_packages : integer range 0 to 3 := 3;
+		begin
+			if (rising_edge(request_new_data)) then
+				if (quant_packages > 0) then
+					is_fifo_empty <= '0';
+					if (quant_packages = 3) then
+						data <= "111011111000";
+					elsif (quant_packages = 2) then
+						data <= "111010101000";
+					elsif (quant_packages = 1) then
+						data <= "100010100101";
+					end if;
+					quant_packages := quant_packages - 1;
+				else
+					is_fifo_empty <= '1';
+				end if;
+			end if;
+		end process;
+		
+		decoder : fm0_decoder port map (
+			clk => clk,
+			rst => '0',
+			enable => '1',
+			clr_err => '0',
+			tari_101 => "0000000111111001", -- (16-len(bin(int(0.99*tari*f))[2:]))*"0" + bin(int(1.584*tari*f))[2:]
+			tari_099 => "0000000111101111",
+			tari_1616 => "0000001100101000",
+			tari_1584 => "0000001100011000", -- freq/tari = clocks cycles
+			data_out => data_out,
+			eop => eop,
+			err => error_out,
+			data_in => encoded_data );
+		
+		encoder : fm0_encoder port map (
+			clk => clk,
+			rst => '0',
+			enable => '1',
+			tari => "0000000111110100", -- tari = 10 us
+			data_out => encoded_data,
+			is_fifo_empty => is_fifo_empty,
+			data_in => data,
+			request_new_data => request_new_data  );
+		
+	   
+		
+	
+end tb;
