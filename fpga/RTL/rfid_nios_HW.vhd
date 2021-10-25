2adf02a960e984d742f33dab6f7f4e516cf3c6f4
Bruno Domingues <brunao.signorelli@hotmail.com>
2021-10-25 20:59:07 -0300

    Formatting

--- fpga/RTL/rfid_nios_HW.vhd
+++ fpga/RTL/rfid_nios_HW.vhd
@@ -1,36 +1,36 @@
-library IEEE;
-use IEEE.std_logic_1164.all;
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
 
-entity rfid_nios_HW is
-	port (
-			-- Gloabals
-			fpga_clk_50 : in std_logic;		-- clock.clk
+ENTITY rfid_nios_HW IS
+    PORT (
+        -- Gloabals
+        fpga_clk_50 : IN STD_LOGIC; -- clock.clk
 
-			-- I/Os
-			pin_rx      : in std_logic;
-			pin_tx		: out std_logic
-	  );
-end entity rfid_nios_HW;
+        -- I/Os
+        pin_rx : IN STD_LOGIC;
+        pin_tx : OUT STD_LOGIC
+    );
+END ENTITY rfid_nios_HW;
 
-architecture hw of rfid_nios_HW is
+ARCHITECTURE hw OF rfid_nios_HW IS
 
-    component RFID_NIOS is
-        port (
-            clk_clk       : in  std_logic := 'X'; -- clk
-            pins_rx       : in  std_logic := 'X'; -- rx
-            pins_tx       : out std_logic;        -- tx
-            reset_reset_n : in  std_logic := 'X'  -- reset_n
+    COMPONENT RFID_NIOS IS
+        PORT (
+            clk_clk : IN STD_LOGIC := 'X'; -- clk
+            pins_rx : IN STD_LOGIC := 'X'; -- rx
+            pins_tx : OUT STD_LOGIC; -- tx
+            reset_reset_n : IN STD_LOGIC := 'X' -- reset_n
         );
-    end component RFID_NIOS;
-	
-begin
+    END COMPONENT RFID_NIOS;
 
-	 u0 : component RFID_NIOS
-		  port map (
-				clk_clk       => fpga_clk_50,       --  clk.clk
-				pins_rx       => pin_rx,       		--  pins.rx
-				pins_tx       => pin_tx,       		--  pins.tx
-				reset_reset_n => '1'					   --  reset.reset_n
-		  );
-		 
-end hw;
+BEGIN
+
+    u0 : COMPONENT RFID_NIOS
+        PORT MAP(
+            clk_clk => fpga_clk_50, --  clk.clk
+            pins_rx => pin_rx, --  pins.rx
+            pins_tx => pin_tx, --  pins.tx
+            reset_reset_n => '1' --  reset.reset_n
+        );
+
+    END hw;
