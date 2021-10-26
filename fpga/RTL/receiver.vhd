5d2051aeb7e990cc3a67f651e8f42393b74f204a
Bruno Domingues <brunao.signorelli@hotmail.com>
2021-10-25 21:52:44 -0300

    Small fix on signal

--- fpga/RTL/receiver.vhd
+++ fpga/RTL/receiver.vhd
@@ -136,7 +136,7 @@
 
         signal data_out_pc : std_logic_vector(31 downto 0) := (others => '0');
         
-        signal write_request, data_ready, eop, data_out_decoder,wrreq : std_logic := '0'; 
+        signal write_request, data_ready, eop, data_out_decoder : std_logic := '0'; 
         
         begin
             fifo : fifo_32_32 port map (
@@ -144,7 +144,7 @@
                 data	=> data_out_pc,
                 rdreq	=> rdreq,
                 sclr	=> sclr,
-                wrreq	=> wrreq,
+                wrreq	=> write_request,
                 empty	=> empty,
                 full	=> full,
                 q		=> data_out_fifo,
